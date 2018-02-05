e24c017201be683f755fca000a85c68c417a4551
T <admin@example.com>
2018-02-05 14:49:39 +0800

    dt

---
+++ src/test20180111/Testmain.java
@@ -0,0 +1,37 @@
+package test20180111;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Testmain {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedOutputStream outp = null;
+		BufferedInputStream in = null;
+		String filenamedownload = "/help/bimportUserInstruction.xls";
+		try {
+			outp = new BufferedOutputStream(null);
+			in = new BufferedInputStream( new FileInputStream(filenamedownload));
+			byte[] b = new byte[1024];
+			int i = 0;
+			while ((i = in.read(b)) > 0) {
+				outp.write(b, 0, i);
+			}
+		} catch (IOException e) {
+ 
+		} finally {
+			if(outp != null){
+				outp.flush();
+				outp.close();
+			}
+			if(in != null){
+				in.close();
+			}
+		}
+ 
+	}
+
+}
